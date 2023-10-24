995f13bc3d8e84dbf8a12459a5ffd8166ff0f961
Harini-S05 <140799279+Harini-S05@users.noreply.github.com>
2023-10-24 20:48:51 +0530

    Create Add_details.dart

---
+++ lib/Add_details.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AddDetailsPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Add Details'),
+      ),
+      body: Center(
+        child: Text('Add your details here.'),
+      ),
+    );
+  }
+}
